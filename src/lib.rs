8ca5b0182e5ac4796ca8f4d9ac704bf88c008b05
Shingo Kato <katomail0510@gmail.com>
2020-04-25 13:28:44 +0000

    Add tokenizer

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,23 @@ extern crate inkwell;
 use inkwell::context::Context;
 use std::{path, process};
 
-pub fn compile(x: u64) {
+fn tokenize(code: String) -> u64 {
+    let mut chars = code.chars().peekable();
+
+    let mut s = String::from("");
+    while let Some(c) = chars.peek() {
+        s += &c.to_string();
+        chars.next();
+    }
+    let num: u64 = s.parse().unwrap_or_else(|err| panic!(err));
+    num
+}
+
+pub fn compile(code: String) {
+    // tokenize
+    let num = tokenize(code);
+
+    // emit
     let context = Context::create();
     let module = context.create_module("rcc4");
     let builder = context.create_builder();
@@ -13,7 +29,7 @@ pub fn compile(x: u64) {
     let basic_block = context.append_basic_block(function, "entry");
     builder.position_at_end(basic_block);
 
-    let const_x = i64_type.const_int(x, false);
+    let const_x = i64_type.const_int(num, false);
     builder.build_return(Some(&const_x));
     let _ = module.print_to_file(path::Path::new("compiled.ll"));
 }
@@ -33,7 +49,7 @@ mod tests {
 
     use self::super::*;
 
-    fn run_test(code: u64, expect: &str) {
+    fn run_test(code: String, expect: &str) {
         compile(code);
         let actual = run();
         println!("{:?} => {:?}", actual, expect);
@@ -42,6 +58,6 @@ mod tests {
 
     #[test]
     fn sigle_number() {
-        run_test(10, "10");
+        run_test("10".to_string(), "10");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate rcc4;
 
 fn main() {
-    rcc4::compile(10);
-    rcc4::run();
+    rcc4::compile("10".to_string());
+    println!("{:?}", rcc4::run());
 }
